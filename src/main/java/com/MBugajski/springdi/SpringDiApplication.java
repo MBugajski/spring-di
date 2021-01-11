60c3f77bd78a2b358544f8d102a18ee5286efc17
MBugajski <michalbugajski@outlook.com>
2021-01-11 23:30:57 +0100

    Init Spring project and Spring context example

---
+++ src/main/java/com/MBugajski/springdi/SpringDiApplication.java
@@ -0,0 +1,23 @@
+package com.MBugajski.springdi;
+
+import com.MBugajski.springdi.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class SpringDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SpringDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
+	}
+
+}
